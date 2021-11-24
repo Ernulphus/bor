e3158421bd18ad4cf68c12e82c8dc554ee19d680
Boaz Kaufman <40705580+Ernulphus@users.noreply.github.com>
2021-11-23 23:07:34 -0500

    Add files via upload

---
+++ precache-manifest.af950441e78b9bdcaab97de557ebe732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b160b1f4bde510b1a42976715cfde99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf3a2152ebe0706279d",
+    "url": "/static/css/main.7e857e36.chunk.css"
+  },
+  {
+    "revision": "4f670508eacb3d6459f8",
+    "url": "/static/js/2.f7395fe3.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.f7395fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f7275efc18b627fd88",
+    "url": "/static/js/3.0d4ad962.chunk.js"
+  },
+  {
+    "revision": "5bf3a2152ebe0706279d",
+    "url": "/static/js/main.7235185b.chunk.js"
+  },
+  {
+    "revision": "0033f45612e5af09b2db",
+    "url": "/static/js/runtime-main.a4955e0e.js"
+  }
+]);
